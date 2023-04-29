6c67623fb6e9f0aa0c957fbd92fd9417ee4b3751
piiivot <the_pivotsk8@hotmail.com>
2023-04-28 19:18:36 -0500

    feat:protafolio example

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-footer>
+    <v-card elevation="0" rounded="0" width="100%" class="bg-grey text-center">
+      <v-card-text>
+        <v-btn class="mw-4" icon="fas fa-comment" variant="plain" color="white">
+        </v-btn>
+        <v-btn class="mw-4" icon="fas fa-university" variant="plain" color="white">
+        </v-btn>
+        <v-btn class="mw-4" icon="fas fa-wifi" variant="plain" color="white">
+        </v-btn>
+        <v-btn class="mw-4" icon="fas fa-podcast" variant="plain" color="white">
+        </v-btn>
+      </v-card-text>
+      <v-card-tex class="text-white">
+        @{{ new Date().getFullYear() }}
+        <strong>rami all rights</strong>
+      </v-card-tex>
+    </v-card>
+  </v-footer>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+.v-footer {
+  padding: 8px 0 !important
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar app color="black" dark flat class="px-12">
+    <v-btn>
+      <v-icon color="yellow" left class="mr-2">fas fa-signature</v-icon> rami portafolio
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text @click='scroll("home")' class="test-yellow">Home</v-btn>
+    <v-btn text @click='scroll("bout ")' class="test-yellow">About</v-btn>
+    <v-btn text @click='scroll("portafolio")' class="test-yellow">Portafolio</v-btn>
+    <v-btn text @click='scroll("service")' class="test-yellow">Service</v-btn>
+    <v-btn text @click='scroll("page")' class="test-yellow">Page</v-btn>
+    <v-btn text @click='scroll("blog")' class="test-yellow">Blog</v-btn>
+    <v-btn text @click='scroll("contact")' class="test-yellow">Contact</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: "smooth" })
+
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
